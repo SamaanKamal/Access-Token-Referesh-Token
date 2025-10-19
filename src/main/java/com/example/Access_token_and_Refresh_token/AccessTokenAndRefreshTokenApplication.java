7136de14c8d2d86215d78a7cce0ae010e6adc145
Samaan Khella (EXT-Nokia) <samaan.khella.ext@nokia.com>
2025-10-19 16:33:04 +0300

    added more roles and users

--- src/main/java/com/example/Access_token_and_Refresh_token/AccessTokenAndRefreshTokenApplication.java
+++ src/main/java/com/example/Access_token_and_Refresh_token/AccessTokenAndRefreshTokenApplication.java
@@ -1,6 +1,7 @@
 package com.example.Access_token_and_Refresh_token;
 
 import com.example.Access_token_and_Refresh_token.Entity.Role;
+import com.example.Access_token_and_Refresh_token.Entity.User;
 import com.example.Access_token_and_Refresh_token.Service.Role.RoleService;
 import com.example.Access_token_and_Refresh_token.Service.User.UserService;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 @PropertySource("classpath:env.properties")
@@ -22,9 +25,21 @@ public class AccessTokenAndRefreshTokenApplication {
         return args -> {
             roleService.saveRole(new Role(null, "USER"));
             roleService.saveRole(new Role(null, "ADMIN"));
+            roleService.saveRole(new Role(null, "MANAGER"));
+            roleService.saveRole(new Role(null, "SUPER_ADMIN"));
 
+            userService.saveUser(new User(null, "John Travolta", "john", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Will Smith", "will", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Arnold Schwarzenegger", "Arnold", "1234", new ArrayList<>()));
 
-
+            userService.addRoleToUser("john", "USER");
+            userService.addRoleToUser("john", "MANAGER");
+            userService.addRoleToUser("will", "MANAGER");
+            userService.addRoleToUser("jim", "ADMIN");
+            userService.addRoleToUser("arnold", "SUPER_ADMIN");
+            userService.addRoleToUser("arnold", "ADMIN");
+            userService.addRoleToUser("arnold", "USER");
         };
     }
 }
--- src/main/java/com/example/Access_token_and_Refresh_token/Utilities/UserDetails.java
+++ src/main/java/com/example/Access_token_and_Refresh_token/Utilities/UserDetails.java
@@ -7,13 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 @RequiredArgsConstructor
 @Slf4j
-public class UserDetails implements UserDetailsService {
+@Component
+public class CustomUserDetails implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
     public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
